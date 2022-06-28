cc171b371c66e60ebf25f6d93d55ee426be239c6
Cristina Diaz <diazcristina.la@gmail.com>
2022-06-27 21:39:04 -0700

    commit tree e280c374ec7c6abf56afd16ca0fa2e3a9ac8288c

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    def index 
+        render json: Student.all
+    end 
+
+    def grades
+        students = Student.order(grade: :desc)
+        render json: students
+    end
+
+    def highest_grade
+        student = Student.order(grade: :desc).first
+        render json: student
+    end 
+end
